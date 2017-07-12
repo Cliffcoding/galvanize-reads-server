1155e7adcf4775d16c71ecab99b26cca4cc9c0c7
Jason Clifton <j.clifton1473@gmail.com>
2017-07-11 18:49:29 -0600

    can add book

--- api/router.js
+++ api/router.js
@@ -1,8 +1,16 @@
 const queries = require('../db/queries');
 const express = require('express');
+const router = express.Router();
 
 router.get('/books', (req,res,next) => {
   queries.getAllBooks().then(books => {
     res.json(books);
-  })
-})
+  });
+});
+router.post('/books/new', (req,res,next) => {
+  queries.createBook(req.body).then(response => {
+    res.json(response);
+  });
+});
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
@@ -16,9 +17,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 
-app.use('api/v1' router);
+app.use('/api/v1', router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
